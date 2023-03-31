d9f3b4e936a15e4780e2c6e938d846dedc18eccb
BogdanDinescu <bogdandinescu@tutanota.com>
2023-03-31 22:36:15 +0300

    Improved Home page performance

--- src/components/HomeComponent.svelte
+++ src/components/HomeComponent.svelte
@@ -1,36 +1,41 @@
 <script lang="ts">
-    import { Paper, Text, Space, Title } from "@svelteuidev/core";
+    import { Space } from "@svelteuidev/core";
     import { onMount } from "svelte";
     import { gun, sea } from "../gunDB";
     import type { Post } from "../Post";
     import PostComponent from "./PostComponent.svelte";
 
-    let followedUsers:{pub:string, alias:string}[] = [];
     let posts: Post[] = []
 
     onMount(() => {
         gun.user().get("following").map().once((alias, pub) => {
             if (alias !== null) {
-                followedUsers.push({pub, alias});
+                gun.user(pub).get("posts").map().once(async (value) => {
+                    const post = JSON.parse(value.substring(3));
+                    const verifiedPost: Post = await sea.verify(value, post.m.pub);
+                    if (verifiedPost) {
+                        posts = pushAndSort(posts, verifiedPost);
+                    } else {
+                        const unverifiedPost = post.m;
+                        unverifiedPost.signingError = true;
+                        posts = pushAndSort(posts, unverifiedPost);
+                    }
+                });
             }
         });
-        followedUsers.forEach((user, key, arr) => {
-            gun.user(user.pub).get("posts").map().once(async (value) => {
-                const post = JSON.parse(value.substring(3));
-                const verifiedPost: Post = await sea.verify(value, post.m.pub);
-                if (verifiedPost) {
-                    posts.push(verifiedPost);
-                } else {
-                    const unverifiedPost = post.m;
-                    unverifiedPost.signingError = true;
-                    posts.push(unverifiedPost);
-                }
-                if (Object.is(arr.length - 1, key)) {
-                    posts = posts.sort((a,b) => b.timestamp - a.timestamp);
-                }
-            });
-        });
     });
+
+    function pushAndSort(arr: Array<Post>, post: Post) {
+        arr.push(post);
+        let i = arr.length - 1;
+        let item = arr[i];
+        while (i > 0 && item.timestamp > arr[i-1].timestamp) {
+            arr[i] = arr[i-1];
+            i -= 1;
+        }
+        arr[i] = item;
+        return arr;
+    }
 </script>
 
 {#each posts as post}
